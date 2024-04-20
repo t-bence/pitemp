c6a18b122f6a2769c68f305a170e550f2a9dc974
t-bence <toth.bence.mihaly@gmail.com>
2024-04-20 21:16:38 +0100

    Send CPU temperature

--- pitemp/temp_sender.py
+++ pitemp/temp_sender.py
@@ -10,7 +10,17 @@ def load_secrets() -> Tuple[str, str]:
         secrets = json.load(f)
     return secrets["CONNECTION_STRING"], secrets["EVENT_HUB_NAME"]
 
+
+def get_cpu_temp() -> float:
+    try:
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as file:
+            return float(file.read().strip()) / 1000
+    except:
+        return 0.0
+
+
 async def run():
+    import time
     # load config from JSON not in git
     conn_str, event_hub_name = load_secrets()
 
@@ -24,10 +34,12 @@ async def run():
         # Create a batch.
         event_data_batch = await producer.create_batch()
 
+        timestamp = time.time()
+
+        data = f"{timestamp},{get_cpu_temp()}"
+
         # Add events to the batch.
-        event_data_batch.add(EventData("First event "))
-        event_data_batch.add(EventData("Second event"))
-        event_data_batch.add(EventData("Third event"))
+        event_data_batch.add(EventData(data))
 
         # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
